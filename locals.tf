d07a54e54c04bc413523d494dc1cb89c5b3b9edb
ulrikof <ulrikof@stud.ntnu.no>
2024-10-21 17:45:41 +0200

    updated names to be locals dependent on workspace

--- locals.tf
+++ locals.tf
@@ -1,4 +1,10 @@
 locals {
-  rg_root_base_name = "uo_rg_root"
-  rg_root_name      = terraform.workspace == "default" ? local.rg_root_base_name : "${local.rg_root_base_name}-${terraform.workspace}"
+  rg_root_base_name    = "uo_rg_root"
+  sql_server_base_name = "uo-sql-server-1"
+  db_base_name         = "uo-db-1"
+
+  rg_root_name    = terraform.workspace == "default" ? local.rg_root_base_name : "${local.rg_root_base_name}-${terraform.workspace}"
+  sql_server_name = terraform.workspace == "default" ? local.sql_server_base_name : "${local.sql_server_base_name}-${terraform.workspace}"
+  db_name         = terraform.workspace == "default" ? local.db_base_name : "${local.db_base_name}-${terraform.workspace}"
+
 }
