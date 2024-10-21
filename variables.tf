530da1f63344df39c22bc7a963c522ac4887e6ad
ulrikof <ulrikof@stud.ntnu.no>
2024-10-21 17:24:19 +0200

    removed unused variables

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "rg_root_name" {
-  type        = string
-  description = "Name of the root resource group"
-}
-variable "location" {
-  type        = string
-  description = "Default location for all services"
-}
