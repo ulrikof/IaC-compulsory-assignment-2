51071becd03841fcc8af3d53c0769a44ae40b117
ulrikof <ulrikof@stud.ntnu.no>
2024-10-21 17:00:34 +0200

    implementing network module in main

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
 resource "azurerm_resource_group" "uo_rg_root" {
-  name     = local.rg_root_name
+  name     = "uo_rg_root"
   location = "West Europe"
 }
 
-#test
+module "virtual_network_module" {
+  source           = "./modules/virtual_network_module"
+  rg_name          = azurerm_resource_group.uo_rg_root.name
+  location         = azurerm_resource_group.uo_rg_root.location
+  vnet_name        = "uo-vnet-1"
+  subnet_name      = "uo-subnet-1"
+  address_prefixes = ["10.0.0.0/16"]
+  address_space    = ["10.0.2.0/24"]
+}
