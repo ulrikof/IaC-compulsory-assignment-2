3a392c1f078b5756176ae991b3f5a443feda3816
ulrikof <ulrikof@stud.ntnu.no>
2024-10-21 16:56:55 +0200

    created a draft for the vnet module

---
+++ modules/virtual_network_module/variables.tf
@@ -0,0 +1,29 @@
+variable "location" {
+  type        = string
+  description = "Location where resource should be placed"
+}
+
+variable "rg_name" {
+  type        = string
+  description = "Name of parent resource group"
+}
+
+variable "vnet_name" {
+  type        = string
+  description = "Name of the vnet"
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "Name of the subnet"
+}
+
+variable "address_space" {
+  type        = list(string)
+  description = "Address space for the vnet"
+}
+
+variable "address_prefixes" {
+  type        = list(string)
+  description = "Address prefixes for the subnet"
+}
