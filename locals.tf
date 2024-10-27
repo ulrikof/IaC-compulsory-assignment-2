fcd79afd230a6c66e3eb3bf12cdb74dc2faa94ff
ulrikof <ulrikof@stud.ntnu.no>
2024-10-27 11:08:44 +0100

    fixed a small problem with db naming

--- locals.tf
+++ locals.tf
@@ -6,5 +6,4 @@ locals {
   rg_root_name    = terraform.workspace == "default" ? local.rg_root_base_name : "${local.rg_root_base_name}-${terraform.workspace}"
   sql_server_name = terraform.workspace == "default" ? local.sql_server_base_name : "${local.sql_server_base_name}-${terraform.workspace}"
   db_name         = terraform.workspace == "default" ? local.db_base_name : "${local.db_base_name}-${terraform.workspace}"
-
 }
