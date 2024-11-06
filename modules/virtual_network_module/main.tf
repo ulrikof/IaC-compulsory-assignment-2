0bf6f2de45dbf0c1db3052cfda356a4a8dbf6d58
ulrikof <ulrikof@stud.ntnu.no>
2024-11-06 13:37:09 +0100

    fixed formatting

--- modules/virtual_network_module/main.tf
+++ modules/virtual_network_module/main.tf
@@ -15,11 +15,11 @@ resource "azurerm_virtual_network" "vnet_1" {
 }
 
 resource "azurerm_subnet" "subnet_1" {
-  resource_group_name = var.rg_name
+  resource_group_name  = var.rg_name
   virtual_network_name = azurerm_virtual_network.vnet_1.name
-  name             = var.subnet_name
-  address_prefixes = ["10.0.2.0/24"]
-  
+  name                 = var.subnet_name
+  address_prefixes     = ["10.0.2.0/24"]
+
 }
 
 resource "azurerm_subnet_network_security_group_association" "subnet_1_nsg" {
