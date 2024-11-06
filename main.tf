cee77336ad311ab34833a785bafc1b80cf73a7d3
ulrikof <ulrikof@stud.ntnu.no>
2024-11-06 14:12:00 +0100

    added missing variable in root module

--- main.tf
+++ main.tf
@@ -38,5 +38,6 @@ module "app_service_module" {
   sa_name           = module.storage_account_module.sa_name
   sa_access_key     = module.storage_account_module.sa_access_key
   sc_name           = module.storage_account_module.sc_name
+  sb_index_name     = module.storage_account_module.sb_index_name
   subnet_id         = module.virtual_network_module.subnet_id
 }
