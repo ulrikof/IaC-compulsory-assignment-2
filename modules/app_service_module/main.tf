9f9672e3dd0141f0324780c1940ee73ac022216c
ulrikof <ulrikof@stud.ntnu.no>
2024-11-06 14:55:05 +0100

    reverting

--- modules/app_service_module/main.tf
+++ modules/app_service_module/main.tf
@@ -8,133 +8,42 @@ terraform {
 }
 
 # Random string used added to the name of sa_01 to make a valid name
-# resource "random_string" "random_string" {
-#   length  = 20
-#   special = false
-# }
+resource "random_string" "random_string" {
+  length  = 20
+  special = false
+}
 
-# resource "azurerm_service_plan" "service_plan_01" {
-#   name                = var.service_plan_name
-#   location            = var.location
-#   resource_group_name = var.rg_name
-#   os_type             = "Linux"
-#   sku_name            = "P1v2"
-# }
-
-# resource "azurerm_linux_web_app" "web_app" {
-#   name                = "${var.app_service_name}${random_string.random_string.result}"
-#   location            = var.location
-#   resource_group_name = var.rg_name
-#   service_plan_id     = azurerm_service_plan.service_plan_01.id
-
-#   site_config {
-#   }
-#   app_settings = {
-#     WEBSITE_RUN_FROM_PACKAGE = "https://${var.sa_name}.blob.core.windows.net/${var.sc_name}/${var.sb_index_name}"
-#   }
-
-#   storage_account {
-#     name         = "sa_01"
-#     account_name = var.sa_name
-#     share_name   = var.sc_name
-#     access_key   = var.sa_access_key
-#     type         = "AzureBlob"
-#   }
-
-#   connection_string {
-#     name  = "Database"
-#     type  = "SQLAzure"
-#     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
-#   }
-# }
-
-resource "azurerm_public_ip" "lb_public_ip" {
-  name                = "lbPublicIP"
+resource "azurerm_service_plan" "service_plan_01" {
+  name                = var.service_plan_name
   location            = var.location
   resource_group_name = var.rg_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+  os_type             = "Linux"
+  sku_name            = "P1v2"
 }
 
-resource "azurerm_lb" "load_balancer" {
-  name                = "myLoadBalancer"
+resource "azurerm_linux_web_app" "web_app" {
+  name                = "${var.app_service_name}${random_string.random_string.result}"
   location            = var.location
   resource_group_name = var.rg_name
-  sku                 = "Standard"
+  service_plan_id     = azurerm_service_plan.service_plan_01.id
 
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.lb_public_ip.id
+  site_config {
   }
-}
-
-resource "azurerm_network_interface" "proxy_nic" {
-  name                = "proxyNic"
-  location            = var.location
-  resource_group_name = var.rg_name
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = var.subnet_id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_lb_backend_address_pool" "lb_backend_pool" {
-  name            = "BackendPool"
-  loadbalancer_id = azurerm_lb.load_balancer.id
-}
-
-resource "azurerm_lb_probe" "http_probe" {
-
-  loadbalancer_id     = azurerm_lb.load_balancer.id
-  name                = "httpProbe"
-  protocol            = "Http"
-  port                = 80
-  request_path        = "/"
-  interval_in_seconds = 5
-  number_of_probes    = 2
-}
-
-resource "azurerm_linux_virtual_machine" "web_vm" {
-  name                  = "webserver-vm"
-  location              = var.location
-  resource_group_name   = var.rg_name
-  network_interface_ids = [azurerm_network_interface.vm_nic.id]
-  size                  = "Standard_B1s"
-
-  admin_username = "adminuser"
-  admin_password = "YourPassword1234!" # Alternatively, use SSH keys
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+  app_settings = {
+    WEBSITE_RUN_FROM_PACKAGE = "https://${var.sa_name}.blob.core.windows.net/${var.sc_name}/${var.sb_index_name}"
   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
+  storage_account {
+    name         = "sa_01"
+    account_name = var.sa_name
+    share_name   = var.sc_name
+    access_key   = var.sa_access_key
+    type         = "AzureBlob"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get install -y docker.io docker-compose git",
-      "sudo usermod -aG docker ${self.admin_username}", # Add user to Docker group
-    ]
+  connection_string {
+    name  = "Database"
+    type  = "SQLAzure"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
-}
-
-resource "azurerm_virtual_machine_extension" "web_vm_extension" {
-  name                 = "webserver-setup"
-  virtual_machine_id   = azurerm_linux_virtual_machine.web_vm.id
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.0"
-  settings             = <<SETTINGS
-    {
-      "commandToExecute": "git clone https://gitlab.stud.idi.ntnu.no/ulrikof/fridge-friend-mirror.git /home/${azurerm_linux_virtual_machine.web_vm.admin_username}/app && cd /home/${azurerm_linux_virtual_machine.web_vm.admin_username}/app && sudo docker-compose up -d"
-    }
-  SETTINGS
 }
