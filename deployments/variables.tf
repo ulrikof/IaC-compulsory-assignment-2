778a9b53b27c29bec360f23a1303c7a315f79498
ulrikof <ulrikof@stud.ntnu.no>
2024-11-10 19:28:32 +0100

    testing if this works

---
+++ deployments/variables.tf
@@ -0,0 +1,19 @@
+variable "sql_server_admin_login" {
+  type = string
+  description = "Username of admin login on the SQL server"
+}
+
+variable "sql_server_admin_password" {
+  type = string
+    description = "Password of admin login on the SQL server"
+}
+
+variable "db_sku" {
+  type = string
+  description = "SKU of the database"
+}
+
+variable "db_max_gb" {
+  type = number
+  description = "Max gb of the database"
+}
