95beb21819f20354dcf5b1810b04e79e8d8c7c3e
ulrikof <ulrikof@stud.ntnu.no>
2024-11-10 18:51:49 +0100

    removed global key vault

--- global/main.tf
+++ global/main.tf
@@ -3,7 +3,6 @@ resource "azurerm_resource_group" "global_rg" {
   location = "West Europe"
 }
 
-data "azurerm_client_config" "current" {}
 
 resource "azurerm_log_analytics_workspace" "global_log_analytics" {
   name                = "global-log-analytics"
@@ -13,16 +12,3 @@ resource "azurerm_log_analytics_workspace" "global_log_analytics" {
   retention_in_days   = 30
 }
 
-resource "azurerm_key_vault" "global_key_vault" {
-  name                = "global-key-vault"
-  location            = azurerm_resource_group.global_rg.location
-  resource_group_name = azurerm_resource_group.global_rg.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-
-  sku_name = "standard"
-
-  network_acls {
-    bypass         = "AzureServices"
-    default_action = "Deny"
-  }
-}
