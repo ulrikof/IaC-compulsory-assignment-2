115e7cd8d96e07b961401c85855951196aa07b20
ulrikof <ulrikof@stud.ntnu.no>
2024-11-10 18:28:11 +0100

    fixed local file

--- deployments/locals.tf
+++ deployments/locals.tf
@@ -1,7 +1,4 @@
 locals {
   rg_root_base_name    = "uo_rg_deployment"
-  sql_server_base_name = "uo-sql-server-1"
-  db_base_name         = "uo-db-1"
-
-  rg_root_name = terraform.workspace == "default" ? local.rg_root_base_name : "${local.rg_root_base_name}-${terraform.workspace}"
+  rg_root_name    = terraform.workspace == "default" ? local.rg_root_base_name : "${local.rg_root_base_name}-${terraform.workspace}"
 }
