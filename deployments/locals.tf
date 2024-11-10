778289b2f7ea8df351b68d52a94e372d89235c3b
ulrikof <ulrikof@stud.ntnu.no>
2024-11-10 17:55:00 +0100

    added some dummy resources to the global file and updated workflows to update this on merge

--- deployments/locals.tf
+++ deployments/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  rg_root_base_name    = "uo_rg_root"
+  rg_root_base_name    = "uo_rg_deployment"
   sql_server_base_name = "uo-sql-server-1"
   db_base_name         = "uo-db-1"
 
---
+++ global/main.tf
@@ -0,0 +1,22 @@
+resource "azurerm_resource_group" "global_rg" {
+  name     = "uo_rg_global"
+  location = "West Europe"
+}
+
+resource "azurerm_log_analytics_workspace" "global_log_analytics" {
+  name                = "global-log-analytics"
+  location            = azurerm_resource_group.global_rg.location
+  resource_group_name = azurerm_resource_group.global_rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+# Example: Shared Key Vault to store secrets for all environments
+resource "azurerm_key_vault" "global_key_vault" {
+  name                = "global-key-vault"
+  location            = azurerm_resource_group.global_rg.location
+  resource_group_name = azurerm_resource_group.global_rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+
+  sku_name = "standard"
+}
