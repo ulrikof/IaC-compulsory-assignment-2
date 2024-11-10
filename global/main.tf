7ff005bf48c0919808cdae10d15cd0467cd67984
ulrikof <ulrikof@stud.ntnu.no>
2024-11-10 21:30:03 +0100

    added a key vault to the global setup

--- global/main.tf
+++ global/main.tf
@@ -12,3 +12,14 @@ resource "azurerm_log_analytics_workspace" "global_log_analytics" {
   retention_in_days   = 30
 }
 
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "key_vault" {
+  name                     = "global-key-vault"
+  location                 = azurerm_resource_group.global_rg.location
+  resource_group_name      = azurerm_resource_group.global_rg.name
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  purge_protection_enabled = false
+  sku_name                 = "standard"
+}
